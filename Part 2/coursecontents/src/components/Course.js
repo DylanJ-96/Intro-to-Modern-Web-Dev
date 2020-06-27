4884a1a1211829b8805ec64e59d05b270fc2844e
DylanJ-96 <62822219+DylanJ-96@users.noreply.github.com>
2020-06-26 18:19:21 -0700

    Course contents step10

--- Part 2/coursecontents/src/components/Course.js
+++ Part 2/coursecontents/src/components/Course.js
@@ -1,17 +1,55 @@
 import React from 'react';
 
-const Course = ({ notes }) => {
-    return (
+const Course = ({ courses }) => {
+
+    const Header = () => (<h1>Web development curriculum</h1>)
+
+    const Content = ({ courses }) => {
+        return (courses.map((c) =>
+            <div key={c.id}>
+                <Parts key={c.id} parts={c} />
+            </div>
+        ))
+    }
+
+    const Parts = ({ parts }) => {
+        return (
+            <div>
+                <Name parts={parts} />
+                <Topics parts={parts} />
+                <Total parts={parts.parts} />
+            </div>
+        )
+    }
+
+    const Name = ({ parts }) => (<h4>{parts.name}</h4>)
+
+    const Topics = ({ parts }) => (
         <div>
-            <h1>Course</h1>
-            <ul>
-                {notes.map(note =>
-                    <li key={note.id}>
-                        {note.content}
-                    </li>)}
-            </ul>
+            {parts.parts.map((x) =>
+                <p key={x.id}>{x.name} {" "} {x.exercises}</p>
+            )}
         </div>
     )
+
+    const Total = ({ parts }) => {
+        var total = parts.reduce((sum, order) => {
+            return sum + order.exercises
+        }, 0)
+        return (
+            <b>
+                total of {total} exercises
+            </b>
+        )
+    }
+
+    return (
+        < div >
+            <Header />
+            <Content courses={courses} />
+        </div >
+    )
+
 }
 
 export default Course;
